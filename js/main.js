9c6bd111d7a35f9b081d6c9b6fca4651051247f5
Stephanie Hayward Johns <hayw0055@algonquinlive.com>
2017-03-13 16:47:22 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var btn = document.querySelector("button");
+var ul = document.querySelector("ul");
+var li, grocery;
+
+function removeItem(ev) {
+
+    "use strict";
+
+    ul.removeChild(ev.target);
+
+}
+
+function addItem() {
+	
+	"use strict";
+
+	grocery = prompt("Enter a new item for grocery list");
+	
+	
+    if (grocery != "") {
+        
+        li = document.createElement("li");
+        
+        li.innerHTML = grocery;
+        
+        ul.appendChild(li);
+        
+        li.addEventListener("click", removeItem, false);
+        
+    }
+}
+
+btn.addEventListener("click", addItem, false);
